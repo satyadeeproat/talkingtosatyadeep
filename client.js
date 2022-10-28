124c85325599ca2a5ced9fe34168c872320e5f08
roat.satyadeep <roat.satyadeep@mollie.com>
2022-10-28 16:02:34 +0200

    sanity client updated

--- client.js
+++ client.js
@@ -3,5 +3,5 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, // or the name you chose in step 1
-  useCdn: true // `false` if you want to ensure fresh data
+  useCdn: false // `false` if you want to ensure fresh data
 })
