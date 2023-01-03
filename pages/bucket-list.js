9e97b6284d14eb6756fb51bb99108de6f1b02f64
roat.satyadeep <roat.satyadeep@mollie.com>
2023-01-03 23:08:02 +0100

    2022-2023 updates

--- pages/bucket-list.js
+++ pages/bucket-list.js
@@ -53,7 +53,7 @@ const BucketList = () => {
 </div>
 <div>
       {hashMap[currentTab].map((item, index) => (
-        <div className="flex flex-row items-baseline border-b border-gray-200 dark:border-gray-800 max-w-3xl w-full mt-8">
+        <div key={index} className="flex flex-row items-baseline border-b border-gray-200 dark:border-gray-800 max-w-3xl w-full mt-8">
         <p className="text-sm font-bold text-gray-400 dark:text-gray-600">
         {index+1}
         </p>
