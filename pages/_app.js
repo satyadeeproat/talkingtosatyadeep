d3389c1cc560e8b9d8ded9d6bce7e76101feb8f1
roat.satyadeep <roat.satyadeep@mollie.com>
2023-09-21 00:24:40 +0200

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,19 @@ import '../styles/globals.css';
 
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from 'next-auth/react';
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App({
   Component,
   pageProps: { session, ...pageProps }
 }) {
   return (
-    <SessionProvider session={session}>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+      <Analytics />
+    </>
   );
 }
